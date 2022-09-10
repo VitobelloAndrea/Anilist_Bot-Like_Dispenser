20500e785fc99f16d7f9d41e6edaa930ccf01b37
VitobelloAndrea <andrea.vitobello@mail.polimi.it>
2022-09-10 12:07:27 +0200

    eliminated some dead code and reorganized the files

--- ThunderstormTheRevenge.py
+++ ThunderstormTheRevenge.py
@@ -5,8 +5,10 @@ client_ID = 000
 client_Secret = "xxx"
 
 
+
 ##################### METHOD: GETTING A PAGE (actually a post message)
 def getPage(pageNumber, token) :
+    import requests
     uri = 'https://graphql.anilist.co'
 
     headerzzPage = {
@@ -47,9 +49,9 @@ def getPage(pageNumber, token) :
     return activities
 
 
-
-############### METHOD: MAKE THE ACTIVITY POST REQUEST
+##################### METHOD: MAKE THE ACTIVITY POST REQUEST
 def postLike(activityNumber, token) :
+    import requests
     uri = 'https://graphql.anilist.co'
 
     headerzz = {
@@ -81,100 +83,101 @@ def postLike(activityNumber, token) :
         print("response : ", response)
     else :
         print(response.text)
-
     #just to check: webbrowser.open_new_tab("https://anilist.co/activity/" + str(activityNumber))
 
 
+##################### METHOD: GET THE PAGE CODE
+def getAuthorizationCode() :
+    import sqlite3
+    con = sqlite3.connect('C:\\Users\\andre\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History')
+    cursor = con.cursor()
+    cursor.execute("SELECT url FROM urls ORDER BY last_visit_time DESC LIMIT 10")
+    urls = cursor.fetchall()
+    #print to check
+    #for i in range(0, len(urls)) :
+    #    print(urls[i])
+    #    print()
 
-import sqlite3
-import webbrowser
-from wsgiref import headers
-con = sqlite3.connect('C:\\Users\\andre\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History')
-cursor = con.cursor()
-cursor.execute("SELECT url FROM urls ORDER BY last_visit_time DESC LIMIT 10")
-urls = cursor.fetchall()
-#print to check
-#for i in range(0, len(urls)) :
-#    print(urls[i])
-#    print()
-
-found = False
-i = 0
-substring = client_redirectURL + "?code="
-while i < len(urls) and not(found):
-    if substring in urls[i][0] :
-        code = urls[i][0].replace(substring, "")
-        found = True
-print(code)
-
-
+    found = False
+    i = 0
+    substring = client_redirectURL + "?code="
+    while i < len(urls) and not(found):
+        if substring in urls[i][0] :
+            code = urls[i][0].replace(substring, "")
+            found = True
+    print(code)
+    return code
 
 
-######################### GETTING THE STUPID ACCESS TOKEN
+##################### METHOD: GETTING THE STUPID ACCESS TOKEN
+def getAccessToken(code) :
+    import requests
+    uri = 'https://anilist.co/api/v2/oauth/token'
 
-import requests
-uri = 'https://anilist.co/api/v2/oauth/token'
+    data = { 
+        "grant_type" : "authorization_code", 
+        "client_id" : client_ID, 
+        "client_secret" : client_Secret, 
+        "redirect_uri" : client_redirectURL, 
+        "code": code
+    }
 
-data = { 
-    "grant_type" : "authorization_code", 
-    "client_id" : client_ID, 
-    "client_secret" : client_Secret, 
-    "redirect_uri" : client_redirectURL, 
-    "code": code
-}
+    headerz = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
 
-headerz = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-}
+    access_token = requests.post(url=uri, json=data, headers=headerz)
+    print(access_token)
+    print(access_token.json()["access_token"])
+    token = access_token.json()["access_token"]
 
-access_token = requests.post(url=uri, json=data, headers=headerz)
-print(access_token)
-print(access_token.json()["access_token"])
-token = access_token.json()["access_token"]
-
+    return token
 
 
+############### METHOD: TIME TO ITERATE! 
+def postActivities(token) :
+    import time
+    pageCounter = 1
+    likeCounter = 0 #count how many likes; maximum of 30 before 1minute time out
+    listCounter = 0 #count the activity of the list that was reached (0 up to 49)
+    continueFlag = True
 
-############### TIME TO ITERATE! 
-import time
+    with open('lastDate.txt', 'r') as file: # extracting the activity epoch up to which the algorithm has run
+        data = file.read().rstrip()
+    epochToReach = int(data) # and converting it to integer
 
-pageCounter = 1
-likeCounter = 0 #count how many likes; maximum of 30 before 1minute time out
-listCounter = 0 #count the activity of the list that was reached (0 up to 49)
-continueFlag = True
+    activities = getPage(pageCounter, token)
+    pageCounter += 1
+    startingEpoch = activities[0].get('createdAt') #getting epoch of the most recent activity
 
-with open('lastDate.txt', 'r') as file: # extracting the activity epoch up to which the algorithm has run
-    data = file.read().rstrip()
-epochToReach = int(data) # and converting it to integer
+    while (continueFlag and pageCounter < 100) :
+        while (listCounter < 50 and continueFlag) :
+            activity = activities[listCounter]
+            if (activity.get('createdAt') < epochToReach) :
+                continueFlag = False
+            elif (likeCounter < 30) :
+                if (not(activity.get('isLiked'))) :
+                    postLike(int(activity.get('id')), token)
+                    likeCounter += 1
+                listCounter += 1
+            else :
+                time.sleep(60)
+                likeCounter = 0
+        if (continueFlag) : 
+            listCounter = 0
+            activities = getPage(pageCounter, token)
+            pageCounter += 1
 
-activities = getPage(pageCounter, token)
-pageCounter += 1
-startingEpoch = activities[0].get('createdAt') #getting epoch of the most recent activity
+    with open('lastDate.txt', "w") as file:
+        file.write(str(startingEpoch))
 
-while (continueFlag and pageCounter < 100) :
-    while (listCounter < 50 and continueFlag) :
-        activity = activities[listCounter]
-        if (activity.get('createdAt') < epochToReach) :
-            continueFlag = False
-        elif (likeCounter < 30) :
-            if (not(activity.get('isLiked'))) :
-                postLike(int(activity.get('id')), token)
-                likeCounter += 1
-            listCounter += 1
-        else :
-            time.sleep(60)
-            likeCounter = 0
-    if (continueFlag) : 
-        listCounter = 0
-        activities = getPage(pageCounter, token)
-        pageCounter += 1
 
-with open('lastDate.txt', "w") as file:
-    file.write(str(startingEpoch))
-
+############### MAIN -> ffs I don't like the __main__
+code = getAuthorizationCode()
+token = getAccessToken(code)
+postActivities(token)
 print("My job is done!")
-
 
 
 """
